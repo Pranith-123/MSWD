fbe74f9d171e068e695e3cd6950bb8e90692f2ac
Pranith-123 <73998864+Pranith-123@users.noreply.github.com>
2021-06-01 10:44:36 +0530

    Update App.js

--- part1/anecdotes/App.js
+++ part1/anecdotes/App.js
@@ -1,49 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+const Button = ({ text, handleClick }) => {
+  return (
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  );
+};
+const Anecdote = ({ anecdote }) => {
+  console.log(anecdote);
+
+  return <p>{anecdote}</p>;
+};
+const BestAnecdote = ({ anecdote, votes }) => {
+  return (
+    <>
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdote} />
+      <Votes votes={votes} />
+    </>
+  );
+};
+const Votes = ({ votes }) => {
+  return <p>has {votes} votes</p>;
+};
 const App = () => {
-
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
 
-  const [selected, setSelected] = useState(0)
-  const [anecdotesRating, setAnecdotesRating] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
-  const indexOfTheAnecdoteWithBiggestRating = anecdotesRating.indexOf( Math.max(...anecdotesRating) );
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
-  function handleAnecdoteRating() {
-    setAnecdotesRating([
-      ...anecdotesRating.slice(0, selected),
-      anecdotesRating[selected] + 1,
-      ...anecdotesRating.slice(selected + 1),
-    ])
-  }
-  
-  console.log(anecdotesRating)
-  
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+  const mostVoted = votes.indexOf(Math.max(...votes));
 
+  const nextAnecdote = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+
+  const addVote = () => {
+    const copy = [...votes]; // Store temporarily
+    copy[selected] += 1;
+    setVotes(copy); // Set copy as new State array
+  };
   return (
     <div>
-      <h2>Anecdote of the day</h2>
-      <div>{anecdotes[selected]}</div>
-      <div>has {anecdotesRating[selected]} votes</div>
-      <br/>
-      <button onClick={ handleAnecdoteRating }>vote</button>
-      <button onClick={() => setSelected(getRandomInt( anecdotes.length )) }>next anecdote</button>
-
-      <h2>Anecdote with most votes</h2>
-      <div>{anecdotes[indexOfTheAnecdoteWithBiggestRating]}</div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} />
+      <Votes votes={votes[selected]} />
+      <Button handleClick={addVote} text="vote" />
+      <Button handleClick={nextAnecdote} text="next anecdote" />
+      <BestAnecdote anecdote={anecdotes[mostVoted]} votes={votes[mostVoted]} />
     </div>
-  )
-}
-
-
+  );
+};
 
 export default App;
