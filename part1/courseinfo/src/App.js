db6038b4df7ce36672aa3f2098b33848f9ba11eb
Pranith-123 <73998864+Pranith-123@users.noreply.github.com>
2021-06-01 10:47:39 +0530

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,35 +1,67 @@
-import React from 'react'
+import React from "react";
 
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+const Header = ({ course }) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+};
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0]}></Part>
+      <Part part={parts[1]}></Part>
+      <Part part={parts[2]}></Part>
+    </>
+  );
+};
+const Part = ({ part }) => {
+  return (
+    <>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
+    </>
+  );
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
