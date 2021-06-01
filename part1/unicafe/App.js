7b4dda44b87adbf48bc437240328191028dfbecd
Pranith-123 <73998864+Pranith-123@users.noreply.github.com>
2021-06-01 10:49:33 +0530

    Update App.js

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -1,26 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Feedback from './components/Feedback'
-import Statistics from './components/Statistics'
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
 
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (total <= 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  const average = (good - bad) / (good + neutral + bad) || 0; // Avoids NaN as default value
+  const positive = (good / total) * 100 || 0;
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              <Title text="Statistics" />
+            </th>
+          </tr>
+          <tr>
+            <Statistic text="good: " value={good}></Statistic>
+          </tr>
+          <tr>
+            <Statistic text="neutral: " value={neutral}></Statistic>
+          </tr>
+          <tr>
+            <Statistic text="bad: " value={bad}></Statistic>
+          </tr>
+          <tr>
+            <Statistic text="all: " value={total}></Statistic>
+          </tr>
+          <tr>
+            <Statistic text="average: " value={average}></Statistic>
+          </tr>
+          <tr>
+            <Statistic text="positive: " value={`${positive} %`}></Statistic>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+const Statistic = ({ text, value }) => {
+  return (
+    <>
+      <td>{text}</td>
+      <td>{value}</td>
+    </>
+  );
+};
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const sumFeedback = good + neutral + bad;
-  const average = (good - bad) / sumFeedback || 0;
-  const persentageOfPositiveFeedbacks = good * 100 / sumFeedback || 0;
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Feedback good={good} neutral={neutral} bad={bad} 
-        setGood={setGood} setNeutral={setNeutral} setBad={setBad} />
-      <Statistics good={good} neutral={neutral} bad={bad} 
-        all={sumFeedback} average={average} persentageOfPositiveFeedbacks={persentageOfPositiveFeedbacks}/>
+      <Title text="give feedback" />
+      <Button text="good" handleClick={() => setGood(good + 1)}></Button>
+      <Button
+        text="neutral"
+        handleClick={() => setNeutral(neutral + 1)}
+      ></Button>
+      <Button text="bad" handleClick={() => setBad(bad + 1)}></Button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
